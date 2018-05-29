48be3a5ccd761d2d4cba3760153e96b1cb4e5c6b
Grant Phelps <gbphelps2@gmail.com>
2018-05-29 12:05:21 -0400

    Develop inertial scroll distortion - commented out for now

---
+++ javascripts.js
@@ -0,0 +1,19 @@
+
+
+
+// let y = 0;
+// let pointer;
+// window.addEventListener('scroll', ()=>{
+//   diff = y - window.scrollY
+//   if(Math.abs(diff) > 50){
+//     const dir = ( diff < 0 ? 'down' : 'up' );
+//     const content = document.getElementsByClassName('content')[0];
+//     content.classList.add(dir)
+//
+//     window.clearTimeout(pointer);
+//     pointer = setTimeout(()=>{
+//       content.classList.remove('up','down')
+//     },100)
+//   }
+//   y = window.scrollY;
+// })
