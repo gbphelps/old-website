14f5a516015c46883ccb35102e4378c8a47cb2f7
Grant Phelps <gbphelps2@gmail.com>
2018-06-06 14:46:33 -0400

    Move over missing files

---
+++ javascripts/components/feature.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class Feature extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div className={`feature ${this.props.feature}`}>
+        <div className={`pane ${this.props.side}`}>
+
+
+          <div className='description'>
+            <h1>{this.props.title}</h1>
+            <div className='line'></div>
+            <p>{this.props.body}</p>
+            <a href={this.props.button.url}className="button">
+              {this.props.button.text}
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+    )
+  }
+}
